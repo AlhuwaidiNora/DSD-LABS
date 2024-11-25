a1f3aa9a172ecaddeaa9b98719e7b786edb9369c
Nora Rashid Alhuwaidi <98814201+AlhuwaidiNora@users.noreply.github.com>
2024-11-25 10:36:06 +0300

    Finish Task3 Lab2

---
+++ Lab2-Task3/project_1/project_1.srcs/sources_1/new/fbga_top.sv
@@ -0,0 +1,45 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 11/25/2024 09:31:45 AM
+// Design Name: 
+// Module Name: fbga_top
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+module fpga_top (
+    input  logic [2:0] SW,  // Switches for inputs A and B
+    output logic [1:0] LED  // LEDs to display the outputs S and C
+);
+
+    // Internal signals to connect to the half_adder module
+    logic a, b, carryIn;
+    logic sum, carryOut;
+
+    assign a = SW[0];
+    assign b = SW[1];
+    assign carryIn = SW[2];
+
+    // Instantiate the half_adder module
+    full_adder full_adder_inst0 (
+        .*
+    );
+
+    // Connect the outputs S and C to the LEDs
+    assign LED[0] = sum;  // Sum output
+    assign LED[1] = carryOut;  // Carry output
+
+endmodule
+
